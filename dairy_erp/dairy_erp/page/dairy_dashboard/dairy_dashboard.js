3df952bafe7573cc292904dd17a53b66725cdd71
Khushal trvedi <khushal.t@indictranstech.com>
2017-12-11 19:18:20 +0530

    [Feature]-Dairy Dashboard

--- dairy_erp/dairy_erp/page/dairy_dashboard/dairy_dashboard.js
+++ dairy_erp/dairy_erp/page/dairy_dashboard/dairy_dashboard.js
@@ -1,8 +1,10 @@
 frappe.pages['dairy-dashboard'].on_page_load = function(wrapper) {
-	
 	var page = frappe.ui.make_app_page({
 		parent: wrapper,
-		title: 'Dashboard',
+		title: 'Dairy Dashboard',
 		single_column: true
 	});
+
+	$(frappe.render_template("dairy_dashboard")).appendTo(page.main);
+
 }

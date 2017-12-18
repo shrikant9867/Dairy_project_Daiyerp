51fe0e91cd22b4076f05a1e65ba97095c9246707
chaitraliw <chaitrali.w@indictranstech.com>
2017-12-18 16:53:47 +0530

    [Feature]c-007 Dairy Dashboard(v1.5)

--- dairy_erp/dairy_erp/page/dairy_dashboard/dairy_dashboard.js
+++ dairy_erp/dairy_erp/page/dairy_dashboard/dairy_dashboard.js
@@ -16,21 +16,55 @@ dashboard = Class.extend({
 		this.page = wrapper.page
 		this.render_address()
 	},
-	render_view : function(){
+	render_address : function(){
 		var me = this;
-		$(frappe.render_template("dairy_dashboard",{"data":me.data || []})).appendTo(me.page.main);
-		$.each(me.data,function(i,d){
-			if(d.address_type == "Head Office"){
-				$('#head-office').hide()
+		frappe.call({
+			method:"dairy_erp.dairy_erp.page.dairy_dashboard.dairy_dashboard.get_data",
+			callback : function(r){
+				me.data = r.message
+				me.render_view()
 			}
 		})
-		$(me.page.main).find(".new_add").on("click",function(){
+	},
+	render_view : function(){
+		var me = this;
+		$(frappe.render_template("dairy_dashboard",{"data":me.data || {}})).appendTo(me.page.main);
+		$.each(me.data,function(i,d){
+				if(d && d.address_type == "Head Office"){
+					$('#head-office').hide()
+				}
+		})
+		$(me.page.main).find("#head-office").on("click",function(){
+			frappe.route_options = {
+				"address_type": "Head Office"
+			};
+			frappe.new_doc("Address")
+		})
+		$(me.page.main).find("#camp-office").on("click",function(){
+			frappe.route_options = {
+				"address_type": "Camp Office"
+			};
+			frappe.new_doc("Address")
+		})
+		$(me.page.main).find("#chilling-centre").on("click",function(){
+			frappe.route_options = {
+				"address_type": "Chilling Centre"
+			};
+			frappe.new_doc("Address")
+		})
+		$(me.page.main).find("#plant").on("click",function(){
+			frappe.route_options = {
+				"address_type": "Plant"
+			};
 			frappe.new_doc("Address")
 		})
 		$(me.page.main).find("#new_vlcc").on("click",function(){
 			frappe.new_doc("Village Level Collection Centre")
 		})
 		$(me.page.main).find("#new_supp").on("click",function(){
+			frappe.route_options = {
+				"supplier_type": "Dairy Local"
+			};
 			frappe.new_doc("Supplier")
 		})
 		$(me.page.main).find(".camp-office").on("click",function(){
@@ -49,15 +83,5 @@ dashboard = Class.extend({
 			frappe.set_route("List", "Supplier", {'supplier_type': "Dairy Local"});
 		})	
 		
-	},
-	render_address : function(){
-		var me = this;
-		frappe.call({
-			method:"dairy_erp.dairy_erp.page.dairy_dashboard.dairy_dashboard.get_data",
-			callback : function(r){
-				me.data = r.message
-					me.render_view()
-			}
-		})
 	}
 })

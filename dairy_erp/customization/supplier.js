836285fcf5ae728f44e8c04b7da1167b8df83080
chaitraliw <chaitrali.w@indictranstech.com>
2018-02-16 15:06:21 +0530

    [Fix] c-055 Added company on creation of vlcc & dairy local supplier

--- dairy_erp/customization/supplier.js
+++ dairy_erp/customization/supplier.js
@@ -5,5 +5,51 @@ frappe.ui.form.on('Supplier', {
 						frappe.set_route("dairy-dashboard");
 					})
 				}
-			}
+	},
+	onload: function(frm){
+		var operator = get_session_user_type()
+		if (operator.operator_type == 'Camp Office'){
+			cur_frm.set_query("supplier_type", function () {
+				return {
+					"filters": {
+						"supplier_type":  ["in",["Dairy Local","Vlcc Type"]]
+					}
+				}
+			});		
+		}
+		else if(operator.operator_type == 'VLCC'){
+			cur_frm.set_query("supplier_type", function () {
+				return {
+					"filters": {
+						"supplier_type":  ["in",['Dairy Type','Farmer','VLCC Local']]
+					}
+				}
+			});	
+		}
+	}
 })
+
+
+get_session_user_type = function() {
+	var user;
+	frappe.call({
+		method: "frappe.client.get_value",
+		args: {
+			doctype: "User",
+			filters: {"name": frappe.session.user},
+			fieldname: ["operator_type","company"]
+		},
+		async:false,
+		callback: function(r){
+			if(r.message){	
+	
+				user = {
+					"operator_type": r.message.operator_type,
+					"company": r.message.company
+				}		
+			}
+		}
+	});
+
+	return user
+}

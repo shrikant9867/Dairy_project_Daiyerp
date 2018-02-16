f19b4130c901775488c6138868e2a6a533c3fb86
patilsangram <sangram.p@indictranstech.com>
2018-02-16 19:51:11 +0530

    [VLCC Dashboard]

--- dairy_erp/dairy_erp/page/vlcc_dashboard/vlcc_dashboard.js
+++ dairy_erp/dairy_erp/page/vlcc_dashboard/vlcc_dashboard.js
@@ -9,7 +9,6 @@ frappe.pages['vlcc-dashboard'].on_page_load = function(wrapper) {
 
 vlcc_dashboard =  Class.extend({
 	init: function(wrapper){
-		console.log("**")
 		var me= this;
 		this.wrapper = wrapper;
 		this.page = wrapper.page
@@ -17,6 +16,129 @@ vlcc_dashboard =  Class.extend({
 	},
 	render_view: function(){
 		var me = this;
-		$(frappe.render_template("vlcc_dashboard")).appendTo(me.page.main);
+		$(frappe.render_template("dashboard_layout")).appendTo(me.page.main);
  	},
+})
+
+frappe.pages['vlcc-dashboard'].on_page_load = function(wrapper) {
+	new frappe.vlcc_dashboard({
+		$wrapper: $(wrapper)
+	});
+	frappe.breadcrumbs.add("Dairy Erp");
+}
+
+frappe.vlcc_dashboard = Class.extend({
+	init: function(opts) {
+		this.$wrapper = opts.$wrapper
+		this.render_layout();
+	},
+
+	render_layout: function() {
+		this.$wrapper.empty();
+		this.$wrapper.append(frappe.render_template("dashboard_layout"));
+		this.$sidebar = this.$wrapper.find("#dairy_sidebar");
+		this.$content = this.$wrapper.find(".dairy_content");
+		this.$total_row = this.$wrapper.find('.total_summery');
+		this.$addresses = this.$wrapper.find('.addresses');
+		this.$party_master = this.$wrapper.find('.party_master');
+		this.make();
+	},
+
+	make: function() {
+		this.make_sidebar();
+		this.fetch_vlcc_data();
+		this.bind_event();
+	},
+
+	make_sidebar: function() {
+		side_menus = [{
+			"label": "VLCC",
+			"doctype": "Village Level Collection Centre",
+			"add_type": ""
+		},
+		{
+			"label": "Head Office",
+			"doctype": "Address",
+			"add_type": "Head Office"
+		},
+		{
+			"label": "Camp Office",
+			"doctype": "Address",
+			"add_type": "Camp Office"
+		},
+		{
+			"label": "Chilling Center",
+			"doctype": "Address",
+			"add_type": "Chilling Centre"
+		},
+		{
+			"label": "Plant Office",
+			"doctype": "Address",
+			"add_type": "Plant"
+		},
+		{
+			"label": "Supplier",
+			"doctype": "Supplier",
+			"add_type": ""
+		},
+		{
+			"label": "Farmer",
+			"doctype": "Farmer",
+			"add_type": ""
+		}]
+		this.$sidebar.append(frappe.render_template('dashboard_sidebar', {'side_menus': side_menus}))
+	},
+
+	fetch_vlcc_data: function() {
+		var me = this;
+		frappe.call({
+			method: "dairy_erp.dairy_erp.page.vlcc_dashboard.vlcc_dashboard.get_vlcc_data",
+			callback: function(r) {
+				me.render_content(r.message)
+				me.bind_event();
+			}
+		})
+	},
+
+	render_content: function(data) {
+		var me = this;
+		// total sumerry row
+		me.$total_row.append(frappe.render_template('total_summery', {
+			'data': data['total_summery']
+		}))
+
+		//addresses
+		$.each(data['addresses'], function(idx, address){
+			me.$addresses.append(frappe.render_template('dashboard_panel', {
+				'data': address
+			}))
+		})
+
+		//farmer
+		me.$party_master.append(frappe.render_template('dashboard_panel', {
+			'data': data['farmer']
+		}))
+
+		//supplier
+		me.$party_master.append(frappe.render_template('dashboard_panel', {
+			'data': data['supplier']
+		}))
+	},
+
+
+	bind_event: function() {
+		//side bar menu click
+		$('._doctype').on("click", function() {
+			doctype = $(this).attr('data-doctype');
+			add_type = $(this).attr('data-add_type');
+			filters =  add_type ? {"address_type": add_type} : {}
+			frappe.set_route("List", doctype, filters)
+		})
+
+		//add new btn
+		$('.new_master').on("click", function() {
+			doctype = $(this).attr('data-doctype');
+			frappe.new_doc(doctype)
+		})
+	}
 })

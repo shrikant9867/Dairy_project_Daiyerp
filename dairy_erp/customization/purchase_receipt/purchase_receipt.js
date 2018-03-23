8403c6238b3572a40823a295c6d5d1c44956c311
patilsangram <sangram.p@indictranstech.com>
2018-03-23 13:32:16 +0530

    [fix removed rejected_qty updation on PR temporary]

--- dairy_erp/customization/purchase_receipt/purchase_receipt.js
+++ dairy_erp/customization/purchase_receipt/purchase_receipt.js
@@ -1,5 +1,5 @@
 
-frappe.ui.form.on("Purchase Receipt Item", {
+/*frappe.ui.form.on("Purchase Receipt Item", {
 	qty: function(doc, cdt, cdn) {
 		var item = frappe.get_doc(cdt, cdn);
 		//original_qty, received_qty, qty, rejected_qty
@@ -20,5 +20,5 @@ frappe.ui.form.on("Purchase Receipt Item", {
 		frappe.model.round_floats_in(item, ["qty", "received_qty", "rejected_qty"]);
 	}
 
-})
+})*/
 $.extend(cur_frm.cscript, new dairy.price_list.PriceListController({frm: cur_frm}));

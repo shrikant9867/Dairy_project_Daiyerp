a96cf0aaa13b5e70e27ad6831ff2d9fe64419a7d
patilsangram <sangram.p@indictranstech.com>
2018-03-28 18:29:42 +0530

    [VMCR transaction record cretion]

--- dairy_erp/dairy_erp/doctype/vlcc_milk_collection_record/vlcc_milk_collection_record.py
+++ dairy_erp/dairy_erp/doctype/vlcc_milk_collection_record/vlcc_milk_collection_record.py
@@ -19,14 +19,24 @@ class VlccMilkCollectionRecord(Document):
 		try:
 			if self.status == "Accept":
 				pr = self.make_purchase_receipt()
-				self.make_delivery_note(pr)
+				dn = self.make_delivery_note_vlcc()
+				pi = self.make_purchase_invoice(pr)
+				si = self.make_sales_invoice(dn)
+				frappe.msgprint(_("Delivery Note <b>{0}</b>, Sales Invoice <b>{1}</b> Created at Vlcc level \
+					AND Purchase Receipt <b>{2}</b>, Purchase Invoice <b>{3}</b> Created at Chilling centre level".format(
+						'<a href="#Form/Delivery Note/'+dn+'">'+dn+'</a>',
+						'<a href="#Form/Sales Invoice/'+si+'">'+si+'</a>',
+						'<a href="#Form/Purchase Receipt/'+pr+'">'+pr+'</a>',
+						'<a href="#Form/Purchase Invoice/'+pi+'">'+pi+'</a>',
+					)))
 		except Exception as e:
+			raise e
 			print frappe.get_traceback()
 			frappe.db.rollback()
-			frappe.msgprint(e)
+			frappe.throw(e)
 
 	def validate_duplicate_entry(self):
-		args = {
+		filters = {
 			"societyid": self.societyid,
 			"collectiontime": self.collectiontime,
 			"collectiondate": self.collectiondate,
@@ -35,6 +45,7 @@ class VlccMilkCollectionRecord(Document):
 			"farmerid": self.farmerid,
 			"milktype": self.milktype
 		}
+		is_duplicate = frappe.db.get_value("Vlcc Milk Collection Record", filters, "name")
 		if is_duplicate and is_duplicate != self.name:
 			frappe.throw(_("Duplicate Entry found - {0}".format(is_duplicate)))
 
@@ -65,15 +76,47 @@ class VlccMilkCollectionRecord(Document):
 				equal to stock {1} available at {2} warehouse".format(item, stock_qty, vlcc_warehouse)))
 
 	def make_purchase_receipt(self):
-		item_mapper = {"COW": "COW Milk", "BUFFALO": "BUFFALO Milk"}
-		camp_office = frappe.db.get_value("Village Level Collection Centre",{'name':self.associated_vlcc},"camp_office")
-		item = frappe.get_doc("Item", item_mapper[self.milktype])
-		pr = frappe.new_doc("Purchase Receipt")
-		pr.supplier =  self.associated_vlcc
-		pr.vlcc_milk_collection_record = self.name
-		pr.company = frappe.db.get_value("Company",{"is_dairy":1},'name')
-		pr.camp_office = camp_office
-		pr.append("items",
+		try:
+			item_mapper = {"COW": "COW Milk", "BUFFALO": "BUFFALO Milk"}
+			camp_office = frappe.db.get_value("Village Level Collection Centre",{'name':self.associated_vlcc},"camp_office")
+			item = frappe.get_doc("Item", item_mapper[self.milktype])
+			pr = frappe.new_doc("Purchase Receipt")
+			pr.supplier =  self.associated_vlcc
+			pr.vlcc_milk_collection_record = self.name
+			pr.company = frappe.db.get_value("Company",{"is_dairy":1},'name')
+			pr.camp_office = camp_office
+			pr.append("items",
+				{
+					"item_code": item.item_code,
+					"item_name": item.item_name,
+					"description": item.description,
+					"uom": "Litre",
+					"qty": self.milkquantity,
+					"rate": self.rate,
+					"amount": self.amount,
+					"warehouse": frappe.db.get_value("Address", {"centre_id": self.societyid }, 'warehouse')
+				}
+			)
+			pr.status = "Completed"
+			pr.per_billed = 100
+			pr.flags.ignore_permissions = True
+			pr.submit()
+			return pr.name
+		except Exception as e:
+			raise e
+
+	def make_delivery_note_vlcc(self):
+		try:
+			item_mapper = {"COW": "COW Milk", "BUFFALO": "BUFFALO Milk"}
+			item = frappe.get_doc("Item", item_mapper[self.milktype])
+			customer = frappe.db.get_value("Village Level Collection Centre", self.associated_vlcc, "plant_office")
+			warehouse = frappe.db.get_value("Village Level Collection Centre", {"amcu_id": self.farmerid }, 'warehouse')
+			cost_center = frappe.db.get_value("Cost Center", {"company": self.associated_vlcc}, 'name')
+			dn = frappe.new_doc("Delivery Note")
+			dn.customer = customer
+			dn.vlcc_milk_collection_record = self.name
+			dn.company = self.associated_vlcc
+			dn.append("items",
 			{
 				"item_code": item.item_code,
 				"item_name": item.item_name,
@@ -82,35 +125,66 @@ class VlccMilkCollectionRecord(Document):
 				"qty": self.milkquantity,
 				"rate": self.rate,
 				"amount": self.amount,
-				"warehouse": frappe.db.get_value("Address", {"centre_id": self.societyid }, 'warehouse')
-			}
-		)
-		pr.status = "Completed"
-		pr.per_billed = 100
-		pr.flags.ignore_permissions = True
-		pr.submit()
+				"warehouse": warehouse,
+				"cost_center": cost_center
+			})
+			dn.status = "Completed"
+			dn.flags.ignore_permissions = True
+			dn.submit()
+			return dn.name
+		except Exception as e:
+			raise e
 
-	def make_delivery_note(self, pr):
-		item_mapper = {"COW": "COW Milk", "BUFFALO": "BUFFALO Milk"}
-		item = frappe.get_doc("Item", item_mapper[self.milktype])
-		customer = frappe.db.get_value("Village Level Collection Centre", self.associated_vlcc, "plant_office")
-		warehouse = frappe.db.get_value("Village Level Collection Centre", {"amcu_id": self.farmerid }, 'warehouse')
-		cost_center = frappe.db.get_value("Cost Center", {"company": self.associated_vlcc}, 'name')
-		dn = frappe.new_doc("Delivery Note")
-		dn.customer = customer
-		dn.vlcc_milk_collection_record = self.name
-		dn.company = self.associated_vlcc
-		dn.append("items", {
-			"item_code": item.item_code,
-			"item_name": item.item_name,
-			"description": item.description,
-			"uom": "Litre",
-			"qty": self.milkquantity,
-			"rate": self.rate,
-			"amount": self.amount,
-			"warehouse": warehouse,
-			"cost_center": cost_center
-		})
-		dn.status = "Completed"
-		dn.flags.ignore_permissions = True
-		dn.submit()
+	def make_purchase_invoice(self,pr):
+		try:
+			item_mapper = {"COW": "COW Milk", "BUFFALO": "BUFFALO Milk"}
+			item = frappe.get_doc("Item", item_mapper[self.milktype])
+			pi = frappe.new_doc("Purchase Invoice")
+			pi.supplier =  "vlcc2"
+			pi.vlcc_milk_collection_record = self.name
+			pi.company = frappe.db.get_value("Company",{"is_dairy":1},'name')
+			pi.append("items",
+				{
+					"item_code": item.item_code,
+					"item_name": item.item_name,
+					"description": item.description,
+					"uom": "Litre",
+					"qty": self.milkquantity,
+					"rate": self.rate,
+					"amount": self.amount,
+					"warehouse": frappe.db.get_value("Village Level Collection Centre", self.associated_vlcc, 'warehouse'),
+					"purchase_receipt": pr
+				}
+			)
+			pi.flags.ignore_permissions = True
+			pi.submit()
+			return pi.name
+		except Exception as e:
+			raise e
+
+	def make_sales_invoice(self, dn):
+		try:
+			item_mapper = {"COW": "COW Milk", "BUFFALO": "BUFFALO Milk"}
+			item = frappe.get_doc("Item", item_mapper[self.milktype])
+			customer = frappe.db.get_value("Village Level Collection Centre", self.associated_vlcc, "plant_office")
+			warehouse = frappe.db.get_value("Village Level Collection Centre", {"amcu_id": self.farmerid }, 'warehouse')
+			cost_center = frappe.db.get_value("Cost Center", {"company": self.associated_vlcc}, 'name')
+			si = frappe.new_doc("Sales Invoice")
+			si.customer = customer
+			si.company = self.associated_vlcc
+			si.vlcc_milk_collection_record = self.name
+			si.append("items",
+			{
+				"item_code": item.item_code,
+				"qty": self.milkquantity,
+				"rate": self.rate,
+				"amount": self.amount,
+				"warehouse": warehouse,
+				"cost_center": cost_center,
+				"delivery_note": dn
+			})
+			si.flags.ignore_permissions = True
+			si.submit()
+			return si.name
+		except Exception as e:
+			raise e

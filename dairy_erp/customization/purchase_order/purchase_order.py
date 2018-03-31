fab07e88704a59294c3fbd1b1a6f0d778d411a9c
patilsangram <sangram.p@indictranstech.com>
2018-03-31 07:16:27 +0530

    [update Chilling centre & is dropship flag on MI from PO]

--- dairy_erp/customization/purchase_order/purchase_order.py
+++ dairy_erp/customization/purchase_order/purchase_order.py
@@ -2,10 +2,10 @@ import frappe
 
 def update_material_indent(doc, method=None):
 	# update is_dropship on material indent if PO is dropship
-	if doc.is_dropship:
+	if doc.is_dropship or doc.chilling_centre:
 		material_req = [ row.material_request for row in doc.items if row.material_request]
 		if len(material_req):
 			mi = "(" + ",".join("'{0}'".format(mr) for mr in set(material_req))  + ")"
-			frappe.db.sql(""" update `tabMaterial Request` set is_dropship = 1 
-				where name in {0} """.format(mi))
+			frappe.db.sql(""" update `tabMaterial Request` set is_dropship = {0} and chilling_centre = {1}
+				where name in {2} """.format(doc.is_dropship, doc.chilling_centre, mi))
 			frappe.db.commit()

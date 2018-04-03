1336aed290e1bb643beabcf4b5bab58e73a0ce3f
patilsangram <sangram.p@indictranstech.com>
2018-04-03 15:47:24 +0530

    [added missing statuses in MI List API]

--- dairy_erp/material_req_api.py
+++ dairy_erp/material_req_api.py
@@ -19,7 +19,7 @@ def get_mr_list(data):
 	
 	response_dict = {}
 	try:
-		mr_list = frappe.db.sql("""select status,name,schedule_date,camp_office from `tabMaterial Request` where company= '{0}' and status ='Pending' order by creation  desc limit 10""".format(get_seesion_company_datails().get('company')),as_dict=1)
+		mr_list = frappe.db.sql("""select status,name,schedule_date,camp_office from `tabMaterial Request` where company= '{0}' and status in ('Pending', 'Partially Delivered', 'Ordered') order by creation  desc limit 10""".format(get_seesion_company_datails().get('company')),as_dict=1)
 		for row in mr_list:
 			row.update({"items": frappe.db.sql("select item_code,item_name,uom, qty from `tabMaterial Request Item` where parent = '{0}'".format(row.get('name')),as_dict=1)})
 		response_dict.update({"status":"success","data":mr_list})

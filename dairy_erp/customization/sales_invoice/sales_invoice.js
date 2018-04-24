6ded1331cacb508700375e7f60f67f061c60c6d4
Khushal Trivedi <khushal.t@indictranstech.com>
2018-04-24 18:43:34 +0530

    [Fix] - update stock readonly for LS,is dropship fix on form onload

--- dairy_erp/customization/sales_invoice/sales_invoice.js
+++ dairy_erp/customization/sales_invoice/sales_invoice.js
@@ -52,6 +52,7 @@ frappe.ui.form.on("Sales Invoice", {
 		if (frm.doc.local_sale){
 			frm.set_df_property("customer", "read_only", 1);
 			frm.set_df_property("due_date", "read_only", 1);
+			frm.set_df_property("update_stock", "read_only", 1);
 			frm.set_value("update_stock",1)
 			local_sale_operations(frm)
 			frm.set_value("due_date",frappe.datetime.nowdate())
@@ -60,6 +61,7 @@ frappe.ui.form.on("Sales Invoice", {
 		else{
 			frm.set_value("customer_or_farmer","Vlcc Local Customer")
 			frm.set_df_property("customer", "read_only", 0);
+			frm.set_df_property("update_stock", "read_only", 1);
 			frm.set_df_property("due_date", "read_only", 0);
 			frm.set_value("update_stock",0)
 			frm.set_value("due_date","")

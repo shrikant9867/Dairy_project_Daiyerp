96c13408758dd34202daa77a6892f4af0e005e8f
patilsangram <sangram.p@indictranstech.com>
2018-05-08 19:37:47 +0530

    [address mandatory on vlcc]

--- dairy_erp/dairy_erp/doctype/village_level_collection_centre/village_level_collection_centre.js
+++ dairy_erp/dairy_erp/doctype/village_level_collection_centre/village_level_collection_centre.js
@@ -14,7 +14,7 @@ frappe.ui.form.on('Village Level Collection Centre', {
 		frm.set_df_property("amcu_id", "read_only", frm.doc.__islocal ? 0:1);
 		frm.set_df_property("name1", "read_only", frm.doc.__islocal ? 0:1);
 		frm.set_df_property("camp_office", "read_only", frm.doc.__islocal ? 0:1);
-		frm.set_df_property("section_break_5","hidden", frm.doc.__islocal ? 1:0)
+		//frm.set_df_property("section_break_5","hidden", frm.doc.__islocal ? 1:0)
 	},
 	
 	onload: function(frm) {

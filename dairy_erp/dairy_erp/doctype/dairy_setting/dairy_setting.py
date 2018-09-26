bda73c8e9c0dbd5d0943aae5d15df1b26eb60b16
Shrikant Dixit <shrikant.d@indictrans.in>
2018-09-26 16:06:47 +0530

    [feature][multiple auto society id creation using csv upload]

--- dairy_erp/dairy_erp/doctype/dairy_setting/dairy_setting.py
+++ dairy_erp/dairy_erp/doctype/dairy_setting/dairy_setting.py
@@ -5,6 +5,91 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-
+import json
+from frappe.utils.csvutils import read_csv_content_from_attached_file
+from frappe.utils import flt, now_datetime, cstr, random_string
 class DairySetting(Document):
 	pass
+
+@frappe.whitelist()
+def get_csv(doc):
+	try:
+		doc = json.loads(doc)
+		count=0
+		max_rows = 500
+		msg,fmcr_msg = "",""
+		rows = read_csv_content_from_attached_file(frappe.get_doc("Dairy Setting",doc.get('name')))
+		print "---------------> CSV 1",rows
+		if not rows:
+			frappe.throw(_("Please select a valid csv file with data"))
+
+		if len(rows) > max_rows:
+			frappe.throw(_("Maximum {0} rows allowed").format(max_rows))
+
+		for row in rows:
+			print "----------------------",row
+			if count != 0:
+						vlcc = frappe.new_doc("Village Level Collection Centre")
+						vlcc.vlcc_name = row[0]
+						vlcc.abbr = row[1]
+						vlcc.name1 = row[2]
+						vlcc.email_id = row[3]
+						vlcc.contact_no = row[4]
+						vlcc.plant_office = row[5]
+						vlcc.camp_office = row[6]
+						vlcc.amcu_id = row[7]
+						vlcc.vlcc_type = row[8]
+						vlcc.global_percent_effective_credit = row[9]
+						vlcc.chilling_centre =row[10]
+						if int(row[11]) == 1:	
+							print row[11],"-----------+++++++++++++++++++++++------------------------+++++++++++++----------"
+							vlcc.operator_same_as_agent = 1
+							vlcc.operator_number = row[12]
+							vlcc.operator_email_id = row[13]
+							vlcc.operator_name = row[14]
+
+						vlcc.flags.ignore_permissions = True	
+						vlcc.save()	
+						addr = frappe.new_doc("Address")
+						addr.address_title = row[15]
+						addr.address_type = "Vlcc"
+						addr.address_line1 = row[16]
+						addr.city = row[17]
+						addr.vlcc = row[0]
+						addr.append("links", {
+								"link_doctype": "Company",
+								"link_name": row[0],
+								
+							})
+						addr.save()
+						vlcc.address = row[0]+"-Vlcc"
+						vlcc.address_display = row[16]
+						vlcc.save()	
+
+						
+			count +=1
+			frappe.msgprint("Record Inserted")			
+	except Exception as e:
+		frappe.db.rollback()
+		make_dairy_log(title="Failed attribute for vlcc creation",method="vlcc_creation", status="Error",data = "data", message=e, traceback=frappe.get_traceback())
+
+
+			
+			
+			
+def make_dairy_log(**kwargs):
+	dlog = frappe.get_doc({"doctype":"Dairy Log"})
+	dlog.update({
+			"title":kwargs.get("title"),
+			"method":kwargs.get("method"),
+			"sync_time": now_datetime(),
+			"status":kwargs.get("status"),
+			"data":kwargs.get("data", ""),
+			"error_message":kwargs.get("message", ""),
+			"traceback":kwargs.get("traceback", ""),
+			"doc_name": kwargs.get("doc_name", "")
+		})
+	dlog.insert(ignore_permissions=True)
+	frappe.db.commit()
+	return dlog.name
+			

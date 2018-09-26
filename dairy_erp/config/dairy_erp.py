d769f1816a2e3217569aacdb40e7bf296cfba0eb
khatrijitendra <jitendra.k@indictranstech.com>
2018-09-26 16:21:10 +0530

    [Fix]Change collectionroute length validation

--- dairy_erp/config/dairy_erp.py
+++ dairy_erp/config/dairy_erp.py
@@ -174,6 +174,13 @@ def get_data():
 				},
 				{
 					"type": "report",
+					"name": "Truck sheet",
+					"label":_("Truck sheet"),
+					"doctype": "Vlcc Milk Collection Record",
+					"is_query_report": True
+				},
+				{
+					"type": "report",
 					"name": "Milk Passbook",
 					"label":_("Milk Passbook"),
 					"doctype": "Farmer Milk Collection Record",

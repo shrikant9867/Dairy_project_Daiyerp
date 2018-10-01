9967efe33f72c9d8cc982c9c4bae16696613bf7c
khatrijitendra <jitendra.k@indictranstech.com>
2018-10-01 13:38:45 +0530

    [Fix]milk bill txt

--- dairy_erp/dairy_erp/doctype/village_level_collection_centre/village_level_collection_centre.py
+++ dairy_erp/dairy_erp/doctype/village_level_collection_centre/village_level_collection_centre.py
@@ -313,6 +313,26 @@ class VillageLevelCollectionCentre(Document):
 					"company": company,
 					"account_name": "Duties and Taxes"
 				}, "name")
+				# loan_and_advance = frappe.db.sql("""
+				# 	select account_name from `tabAccount` 
+				# 	where parent_account like 'Loans and Advances (Assets) - %'
+				# 	and company = '{0}'
+				# 	""".format(company), as_dict=True)
+				# if not frappe.db.get_value("Account", {
+				# 							"company": company,
+				# 							"account_name": loan_and_advance.get('account_name')
+				# 						}, "name"):
+				# 	account = frappe.new_doc("Account")
+				# 	account.update({
+				# 		"company": company,
+				# 		"account_name": "Feed And Fodder Advance",
+				# 		"parent_account": "Loans and Advances (Assets) - "+"AT"+
+				# 		"root_type": "Asset",
+				# 		"account_type": "Tax"
+				# 	})
+				# 	account.flags.ignore_permissions = True
+				# 	account.save()
+
 				if vlcc_duties_acc:
 					for acc in duties_and_taxes_acc:
 						if not frappe.db.get_value("Account", {

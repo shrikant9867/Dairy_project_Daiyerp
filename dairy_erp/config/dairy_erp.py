0d40ee4786cbbdb4c1429a12aa072d7b52a5adb3
Khushal Trivedi <khushal.t@indictranstech.com>
2018-10-06 09:46:29 +0530

    [changes after pull]

--- dairy_erp/config/dairy_erp.py
+++ dairy_erp/config/dairy_erp.py
@@ -205,6 +205,13 @@ def get_data():
 					"description": _(" "),
 				},
 				{
+					"type": "report",
+					"name": "Individual Farmer Payment Settlement",
+					"label": _("Individual Farmer Payment Settlement Report"),
+					"doctype": "Farmer Payment Log",
+					"is_query_report":True
+				},
+				{
 					"type": "page",
 					"name": "daily-milk-purchase",
 					"label": _("Daily Milk Purchase Report"),

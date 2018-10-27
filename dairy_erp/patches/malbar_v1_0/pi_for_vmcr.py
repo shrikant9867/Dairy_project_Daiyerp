30d907ce132abef95827175715dbf33c451cb0e0
Sagar Gharge <sagar.g@indictrans.in>
2018-10-27 16:22:30 +0530

    [Fix] -cleared excess from warehouse patch

--- dairy_erp/patches/malbar_v1_0/pi_for_vmcr.py
+++ dairy_erp/patches/malbar_v1_0/pi_for_vmcr.py
@@ -3,7 +3,3 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-	# created_vmcr_list = frappe.get_all("Vlcc Milk Collection Record", {'societyid': "3000"}, 'name')
-	# for vmcr in created_vmcr_list:
-	# 	print"\n VMCR \t",vmcr.get('name')
-	# 	if frappe.db.exists("Purchase Receipt", {'':})

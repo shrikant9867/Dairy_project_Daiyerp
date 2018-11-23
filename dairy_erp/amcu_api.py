635d34115f0eb2081b6835a190eda4971dbfb99f
Shrikant Dixit <shrikant.d@indictrans.in>
2018-11-23 12:19:44 +0530

    [fix] single copyright

--- dairy_erp/amcu_api.py
+++ dairy_erp/amcu_api.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2018, Stellapps Technologies Private Ltd.
-# Copyright (c) 2018, Stellapps Technologies Private Ltd.
-# Copyright (c) 2018, Stellapps Technologies Private Ltd.
 # For license information, please see license.txt
 # Author Khushal Trivedi
 
